0b7484471e142b7fe3fad2501940bfcdd967b6ab
grrayya <grrayya11@gmail.com>
2026-07-17 23:58:40 -0400

    Clean up engine.py by removing comments
    Removed unnecessary comments and fixed indentation.

--- engine.py
+++ engine.py
@@ -1,16 +1,16 @@
 from abc import ABC, abstractmethod
 
-# --- The Abstraction ---
+
 class IEngine(ABC):
-    """Abstract interface that both the F1Car and the concrete engines depend on."""
     @abstractmethod
     def get_power_output(self) -> str:
         pass
 
-# --- Low-Level Concrete Modules ---
+
 class HondaEngine(IEngine):
     def get_power_output(self) -> str:
         return "Honda RBPT (1000 HP)"
+
 
 class MercedesEngine(IEngine):
     def get_power_output(self) -> str:
